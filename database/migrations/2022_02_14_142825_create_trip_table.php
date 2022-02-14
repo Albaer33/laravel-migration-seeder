58f27db235e80c7a5e74e43776b8e92a63b2b4f3
Albaer33 <alisi3396@gmail.com>
2022-02-14 15:48:47 +0100

    migration done, created trip table in db

---
+++ database/migrations/2022_02_14_142825_create_trip_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trip', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 50);
+            $table->date('trip_departure_date');
+            $table->date('trip_return_date');
+            $table->decimal('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trip');
+    }
+}
