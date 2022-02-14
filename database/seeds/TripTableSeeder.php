37656c056b64c9e51848c91fdeac04c72297f520
Albaer33 <alisi3396@gmail.com>
2022-02-14 16:24:12 +0100

    seedere generated

---
+++ database/seeds/TripTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Trip;
+use Faker\Generator as Faker;
+
+class TripTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 20; $i++) {
+            $new_trip = new Trip();
+            $new_trip->destination = $faker->state();
+            $new_trip->trip_departure_date = $faker->dateTime();
+            $new_trip->trip_return_date = $faker->dateTime();
+            $new_trip->price = $faker->randomFloat(2, 50, 8000);
+            $new_journey->save();
+        }
+    }
+}
